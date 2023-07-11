9adb567122debb808660666b84562a7f20762555
Gudkova Kateryna <kikigudkova@gmail.com>
2023-07-11 08:59:20 +0300

    js code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const numberOfHours = Number(prompt(`Enter the number of hours:`));
+
+const numberOfSeconds = numberOfHours * 3600;
+
+alert(`The number of seconds in ${numberOfHours} hours is ${numberOfSeconds}`);
